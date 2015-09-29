347141bcc0c1c387a7ea28f84e3a0060ab9d627d
Jeremy Morgan <jeremymorgan@gmail.com>
2015-09-29 09:23:38 -0700

    Changed config file

--- app/app.modules.js
+++ app/app.modules.js
@@ -9,17 +9,10 @@ angular
         'angularstrapControllers'
     ]);
 
-
-
 // set of config options stored in constant
-
 angular.module('angularstrapApp')
     .constant('CONFIG', {
     DebugMode: true,
     StepCounter: 0,
     APIHost: 'http://localhost:12017'
-    //  APIHost: 'http://localhost:12017'
-    //  APIHost: 'http://RGSWEB01:9999'
-    //  APIHost: 'http://RG-P-BLU1:9999'
-    //  APIHost: 'http://dev01jmorg01/api'
 });
