a7d331023e1077beb83143d8c5e90b95cd877122
jeremymorgan <jeremymorgan@gmail.com>
2016-07-02 20:32:13 -0700

    added in some real ajax

--- app/components/home/services/home.services.js
+++ app/components/home/services/home.services.js
@@ -6,28 +6,46 @@
 
     asyncService.$inject = ['$http', '$q'];
 
-        function asyncService($http, $q) {
-            
-            var factory = {
-                //properties
-                retrievedData: [],
-                getHeroText : getHeroText
+    function asyncService($http, $q) {
+
+        var factory = {
+            //properties
+            heroData: [],
+            getHeroText: getHeroText,
+            getColumnData: getColumnData
+        };
+
+        function getHeroText() {
+
+            var defer = $q.defer()
+
+            factory.heroData = {
+                HeroHeader: "Hello AngularStrap!",
+                HeroText: "This is the AngularStrap home page. This text is being pulled from a service, and can be populated by hand coding the property in the controller, dynamically or via services."
             };
+            // this will always resolve because this is hardcoded
+            defer.resolve();
 
-            function getHeroText() {
-
-                // this is where we'd put some ajax calls
-
-                factory.retrievedData = {
-                    HeroHeader: "Hello AngularStrap!",
-                    HeroText: "This is the AngularStrap home page. This text is being pulled from a service, and can be populated by hand coding the property in the controller, dynamically or via services."
-                };
-
-                //factory.retrievedData.HeroHeader = "Hello World!";
-                //factory.retrievedData.HeroText = "This is the AngularStrap home page. This text is being pulled from a service, and can be populated by hand coding the property in the controller, dynamically or via services.";
-
-
-            }
-            return factory;
+            return defer.promise
         }
+
+        function getColumnData(columnum) {
+
+            var defer = $q.defer()
+
+            $http({
+                method: 'GET',
+                url: "http://jsonplaceholder.typicode.com" + "/posts/" + columnum
+
+            }).then(function successCallback(response) {
+                defer.resolve(response.data);
+            }, function errorCallback(response) {
+                defer.reject(response);
+            });
+
+            return defer.promise
+        }
+
+        return factory;
+    }
 })();
