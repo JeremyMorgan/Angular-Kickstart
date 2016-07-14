4ebfc16f18a1f047f5dd70bd158474e522309d57
Jeremy Morgan <jmorgan@sureid.com>
2016-07-14 15:19:53 -0700

    final touches

--- app/components/home/home.directives.js
+++ app/components/home/home.directives.js
@@ -1,11 +1,11 @@
 angular.module('angularstrapApp')
     .directive('heroBlock', function() {
         return {
-            templateUrl: '/app/components/home/views/heroblock.view.html'
+            templateUrl: 'http://jeremymorgan.github.io/Angular-Kickstart/app/components/home/views/heroblock.view.html'
         };
     })
     .directive('subSection', function() {
         return {
-            templateUrl: '/app/components/home/views/subsection.view.html'
+            templateUrl: 'http://jeremymorgan.github.io/Angular-Kickstart/app/components/home/views/subsection.view.html'
         };
 });
